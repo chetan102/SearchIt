8e3e284668acf298abb26cce4beb1104ce2b1762
chetan102 <chetanvashisht001@gmail.com>
2022-05-02 16:55:04 +0530

    again commit

---
+++ src/Main/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <div>
+        <iframe src="https://giphy.com/embed/xTkcEQACH24SMPxIQg" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/hand-bored-waiting-xTkcEQACH24SMPxIQg"></a></p>
+    </div>
+  )
+}

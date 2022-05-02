3b081347cc55d97968947073b689132f7b81d29d
chetan102 <chetanvashisht001@gmail.com>
2022-05-02 16:48:51 +0530

    Direct commit

--- src/Main/Card.js
+++ src/Main/Card.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Card({result}) {
     return (
-        <section>
-            <p>{result.title}</p> 
+        <section className='card' >
+           <p className='card-p'><a href={result.url}>{result.url}</a></p>
+           <h3 className='card-h'><a href={result.url}>{result.title}</a></h3>
+           <p className='card-d'>{result.description}</p>
 
         </section>
     )
